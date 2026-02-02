01a0080c8fc5902e4fcc8cd5865ed830445fda05
aaron <aaron@iu>
2026-02-02 18:01:24 +0100

    collections

---
+++ Collections/Program.cs
@@ -0,0 +1,48 @@
+﻿
+int maxInt = MyCompare.Maximum(0, 1);
+
+
+string maxStr = MyCompare.Maximum("Anna", "Zoe");
+
+var s1 = new Student();
+s1.Id = 1;
+
+var s2 = new Student();
+s2.Id = 2;
+
+Student highestStudent = MyCompare.Maximum(s1, s2);
+Console.WriteLine(highestStudent.Id);
+class MyStack<T>
+{
+    private T[] _items = new T[10];
+    public void Push(T item)
+    {
+        _items[0] = item;
+    }
+
+    public T Pop()
+    {
+        return _items[0];
+    }
+}
+
+class Student : IComparable<Student>
+{
+    public int Id { get; set; }
+
+    public int CompareTo(Student? other)
+    {
+        if(other == null) return 1;
+        if (this.Id  < other.Id) return -1;
+        if (this.Id == other.Id) return 0;
+        return 1;
+    }
+}
+
+class MyCompare
+{
+    public static T Maximum<T>(T a, T b) where T : IComparable<T>
+    {
+        return a.CompareTo(b) > 0 ? a : b;
+    }
+}
